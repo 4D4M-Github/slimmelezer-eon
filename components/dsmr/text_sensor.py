8c352ba02f2394cb8e089b8d279fa07c13c1db31
4D4M <123521171+4D4M-Github@users.noreply.github.com>
2025-05-25 09:51:31 +0200

    Update text_sensor.py

--- components/dsmr/text_sensor.py
+++ components/dsmr/text_sensor.py
@@ -11,81 +11,20 @@ AUTO_LOAD = ["dsmr"]
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_DSMR_ID): cv.use_id(Dsmr),
-        cv.Optional("identification"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("p1_version"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("p1_version_be"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("timestamp"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("breaker_status"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("breaker_status_b"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("electricity_failure_log"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("message_short"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("message_long"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("gas_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("thermal_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("water_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("sub_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("gas_delivered_text"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
+        cv.Optional("identification"): text_sensor.text_sensor_schema(),
+        cv.Optional("p1_version"): text_sensor.text_sensor_schema(),
+        cv.Optional("p1_version_be"): text_sensor.text_sensor_schema(),
+        cv.Optional("timestamp"): text_sensor.text_sensor_schema(),
+        cv.Optional("electricity_tariff"): text_sensor.text_sensor_schema(),
+        cv.Optional("electricity_failure_log"): text_sensor.text_sensor_schema(),
+        cv.Optional("message_short"): text_sensor.text_sensor_schema(),
+        cv.Optional("message_long"): text_sensor.text_sensor_schema(),
+        cv.Optional("gas_equipment_id"): text_sensor.text_sensor_schema(),
+        cv.Optional("thermal_equipment_id"): text_sensor.text_sensor_schema(),
+        cv.Optional("water_equipment_id"): text_sensor.text_sensor_schema(),
+        cv.Optional("sub_equipment_id"): text_sensor.text_sensor_schema(),
+        cv.Optional("gas_delivered_text"): text_sensor.text_sensor_schema(),
+        cv.Optional("telegram"): text_sensor.text_sensor_schema().extend({cv.Optional(CONF_INTERNAL, default=True): cv.boolean),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -102,6 +41,9 @@ async def to_code(config):
             var = cg.new_Pvariable(conf[CONF_ID])
             await text_sensor.register_text_sensor(var, conf)
             cg.add(getattr(hub, f"set_{key}")(var))
+            if key != "telegram":
+                # telegram is not handled by dsmr
+                text_sensors.append(f"F({key})")
             text_sensors.append(f"F({key})")
 
     if text_sensors:
